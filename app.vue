a9dba67661e1b386b88e2495eae7c84aa1d60c1a
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2025-05-25 22:04:54 -0300

    feat: adiciona fonte

--- app.vue
+++ app.vue
@@ -1,7 +1,24 @@
+<template>
+  <NuxtPage />
+</template>
+
 <script setup lang="ts">
 import "~/assets/css/tailwind.css";
 </script>
 
-<template>
-  <NuxtPage />
-</template>
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: sans-serif;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+  font-family: "FarAwayGalaxy", sans-serif;
+}
+</style>
