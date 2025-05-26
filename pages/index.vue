763f9b07e519486f3f94fa97bcfa0b3db547de35
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2025-05-25 22:31:45 -0300

    chore: altera o tailwind para a versão 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <h1 class="text-4xl font-bold text-blue-500">🚀 Tailwind funcionando!</h1>
+    <h1 class="text-4xl font-bold text-blue-500">Tailwind funcionando!</h1>
   </div>
 </template>
